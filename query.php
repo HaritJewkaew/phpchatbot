8191916dc92e7362e345f44147be18920161e981
HARIT JEWKAEW <6431503069@lamduan.mfu.ac.th>
2024-09-08 23:08:33 +0700

    UP QUERY

--- query.php
+++ query.php
@@ -25,21 +25,6 @@ function getRequestResult() {
         4 => 'ด่วน',      
         5 => 'ด่วนมาก'    
     );
-
-    $mappingDepcodeType = array(
-        003 => '43 ตรวจสุขภาพ',
-        005 => '42 ตรวจสุขภาพ',
-        007 => '120 ห้องLA',
-        010 => '14 ห้องฉุกเฉิน',
-        032 => '140 X-RAY',
-        047 => '50 ผู้ป่วย',
-        024 => '08 เเพทย์เเผนไทย',
-        027 => '35 ห้องไต',
-        070 => '99 กลับบ้าน',
-        126 => 'หอผู้ป่วย',
-        157 => 'ward อายุรกรรม',
-        170 => '002 ซักประวัติ'
-    );
     
     $stcType = array(
         1 => 'นอน',
@@ -52,12 +37,11 @@ function getRequestResult() {
     $sql = "SELECT * FROM stretcher_register WHERE stretcher_priority_id = 1";
     error_log("Executing query: $sql");
 
-    $sql = "SELECT kskdepartment1.department AS send_department, kskdepartment2.department AS from_department, stretcher_type.stretcher_type_name FROM stretcher_register JOIN kskdepartment AS kskdepartment1 ON kskdepartment1.depcode = stretcher_register.send_depcode JOIN kskdepartment AS kskdepartment2 ON kskdepartment2.depcode = stretcher_register.from_depcode JOIN stretcher_type ON stretcher_type.stretcher_type_id = stretcher_register.stretcher_type_id";
+
+    $sql = "SELECT * FROM kskdepartment WHERE depcode = ";
     error_log("Executing query: $sql");
 
-    $sql = "SELECT stretcher_request_staff.name FROM stretcher_request_staff JOIN stretcher_register ON stretcher_register.ผู้รับ = stretcher_request_staff.Line_name";
-    error_log("Executing query: $sql");
-    
+
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $result = $stmt->get_result();
@@ -72,9 +56,9 @@ function getRequestResult() {
                     $requestResult['ID'] = $row['stretcher_register_id'];
                     $requestResult['Caller'] = $row['doctor_request'];
                     $requestResult['status'] = $typeMapping[$row['stretcher_work_status_id']];
-                    $requestResult['location'] = $mappingDepcodeType[$row['from_depcode']];
+                    $requestResult['location'] = $row['from_depcode'];
                     $requestResult['Type'] = $stcType[$row['stretcher_type_id']];
-                    $requestResult['locations'] = $mappingDepcodeType[$row['send_depcode']];
+                    $requestResult['locations'] = $row['send_depcode'];
                     $requestResult['Patient'] = $row['hn'];
                     $requestResult['reciver'] = $row['ผู้รับ'];
                     $requestResult['stc_send_time'] = $row['เวลารับ'];
