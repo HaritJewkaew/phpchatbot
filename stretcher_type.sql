b6ec79e517662556e2642ed9fda96bcc9d20f288
HaritJewkaew <97998193+HaritJewkaew@users.noreply.github.com>
2024-10-04 18:20:50 +0700

    Delete stretcher_type.sql

--- stretcher_type.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE stretcher_type(stretcher_type_id DOUBLE,stretcher_type_name VARCHAR(10),stretcher_type_active VARCHAR(10),stretcher_type_o2tube_chk VARCHAR(10));
-INSERT INTO stretcher_type (stretcher_type_id,stretcher_type_name,stretcher_type_active,stretcher_type_o2tube_chk) VALUES 
-(1,'นอน','Y',''),
-(3,'นั่ง','Y',''),
-(4,'นั่ง(มีล้อแล้ว)','Y',''),
-(5,'ล้อเข็นนอนออกซิเจน','Y','Y');
